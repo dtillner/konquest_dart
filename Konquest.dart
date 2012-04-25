7edc78600ff4d5be6b6805fbed042e51bac50887
Daniel Tillner <dtillner@gmail.com>
2012-04-25 17:59:31 +0200

    info text

--- Konquest.dart
+++ Konquest.dart
@@ -1,8 +1,70 @@
 #import('dart:dom');
 #source('Vector2D.dart');
 
+final String svgNS = 'http://www.w3.org/2000/svg';
+
+class TextField {
+    SVGElement _node;
+    Vector2D _position;
+    List<String> _lines;
+    List<SVGElement> _lineNodes;
+    String _color;
+    String _font;
+    num _size;
+
+    TextField(num lines) {
+        _node = document.createElementNS(svgNS, 'text');
+        document.documentElement.appendChild(_node);
+        
+        _lines = new List<String>(lines);
+        _lineNodes = new List<SVGElement>(lines);
+        
+        for(int i=0; i<_lineNodes.length; i++) {
+            _lineNodes[i] = document.createElementNS(svgNS, 'tspan');
+            _node.appendChild(_lineNodes[i]);
+        }
+    }
+    
+    Vector2D get position() => _position;
+    set position(Vector2D position) {
+        _position = position;
+
+        for(int i=0; i<_lineNodes.length; i++) {
+            _lineNodes[i].setAttribute('x', _position.x.toString());
+        }
+        _node.setAttribute('y', _position.y.toString());
+    }
+    
+    String getLine(num line) => _lines[line];
+    setLine(num line, String text) {
+        _lines[line] = text;
+        _node.childNodes[line].textContent = text;
+    }
+    
+    String get color() => _color;
+    set color(String color) {
+        _color = color;
+        _node.setAttribute('style', 'fill:$_color; font-family:$_font; font-size:$_size;');
+    }
+    
+    String get font() => _font;
+    set font(String font) {
+        _font = font;
+        _node.setAttribute('style', 'fill:$_color; font-family:$_font; font-size:$_size;');
+    }
+    
+    num get size() => _size;
+    set size(String size) {
+        _size = size;
+        _node.setAttribute('style', 'fill:$_color; font-family:$_font; font-size:$_size;');
+        
+        for(int i=0; i<_lineNodes.length; i++) {
+            _lineNodes[i].setAttribute('dy', '${i*size}');
+        }
+    }
+}
+
 class Circle {
-    final String svgNS = 'http://www.w3.org/2000/svg';
     SVGElement _node;
     Map _values;
     
@@ -40,14 +102,51 @@ class Circle {
 }
 
 class Planet extends Circle {
+    TextField _text;
+    int _ships;
+    int _player;
     num rotation;
+
+    Planet() {
+        _text = new TextField(2);
+        //TODO: player color;
+        _text.color = 'red';
+        _text.font = 'Iceland';
+    }
+    
+    set size(num size) {
+        super.size = size;
+        //TODO: correct font size and position;
+        _text.size = 15;
+    }
+    
+    set center(Vector2D center) {
+        super.center = center;
+        //TODO: correct font size and position;
+        _text.position = super.center;
+        _text.size = 15;
+    }
+    
+    int get player() => _player;
+    set player(int player) {
+        _player = player;
+        _text.setLine(0, 'Player: $player');
+    }
+    
+    int get ships() => _ships;
+    set ships(int ships) {
+        _ships = ships;
+        _text.setLine(1, 'Ships: $ships');
+    }
 }
 
 class Konquest {
     Vector2D screenSize;
-    Circle sun;
-    List<Planet> planets;
+
     List<Circle> grid;
+    List<Circle> stars;
+    List<Planet> planets;
+    Circle sun;
     
     Konquest() {
         screenSize = new Vector2D(window.innerWidth, window.innerHeight);
@@ -59,8 +158,16 @@ class Konquest {
         for(int i=0; i<grid.length; i++) {
             grid[i] = new Circle();
             grid[i].center = screenSize/2;
-            grid[i].size = outerCircle / 100 * (25 * (4-i));
+            grid[i].size = (outerCircle / 100) * (25 * (4-i));
             grid[i].stroke = 'green';
+        }
+
+        stars = new List<Circle>(50);
+        for(int i=0; i<stars.length; i++) {
+            stars[i] = new Circle();
+            stars[i].center = new Vector2D(Math.random() * screenSize.x, Math.random() * screenSize.y);
+            stars[i].size = outerCircle / 250;
+            stars[i].style = 'fill:white';
         }
         
         planets = new List<Planet>(10);
@@ -75,8 +182,10 @@ class Konquest {
             planets[i].size = Math.random() * (outerCircle / 80) + (outerCircle / 80);
             planets[i].style = 'fill:url(#gradient_white_grey)';
             planets[i].rotation = Math.random() / 3;
+            planets[i].player = 1;
+            planets[i].ships = 5;
         }
-        
+
         sun = new Circle();
         sun.center = screenSize / 2;
         sun.size = outerCircle / 100 * 10;
@@ -101,13 +210,18 @@ class Konquest {
             grid[i].size *= maxScale;
         }
         
+        for(int i=0; i<stars.length; i++) {
+            stars[i].center *= scale;
+            stars[i].size *= maxScale;
+        }
+        
         for(int i=0; i<planets.length; i++) {
             planets[i].center -= oldScreenSize / 2;
             planets[i].center *= maxScale;
             planets[i].size *= maxScale;
             planets[i].center += screenSize / 2;
         }
-        
+
         sun.center *= scale;
         sun.size *= maxScale;
     }
